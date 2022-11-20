be518067f5d7090ee3c4b3798a467c0cfbc11f5c
Oleksandr Zozulia <freemanlex@gmail.com>
2022-11-20 21:12:02 +0200

    first commit

---
+++ verification/tests.py
@@ -0,0 +1,64 @@
+"""
+TESTS is a dict with all of your tests.
+Keys for this will be the categories' names.
+Each test is a dict with
+    "input" -- input data for a user function
+    "answer" -- your right answer
+    "explanation" -- not necessarily a key, it's used for an additional info in animation.
+"""
+
+
+TESTS = {
+    "Basics": [
+        {
+            "input": ['world', 'w', 'o'],
+            "answer": True
+        },
+        {
+            "input": ['world', 'w', 'r'],
+            "answer": False
+        },
+        {
+            "input": ['world', 'l', 'o'],
+            "answer": False
+        },
+        {
+            "input": ['list', 'l', 'o'],
+            "answer": False
+        },
+        {
+            "input": ['', 'l', 'o'],
+            "answer": False
+        },
+        {
+            "input": ['list', 'l', 'l'],
+            "answer": False
+        },
+        {
+            "input": ['world', 'd', 'w'],
+            "answer": False
+        },
+    ],
+    "Extra": [
+        {
+            "input": ['cable', 'l', 'b'],
+            "answer": False
+        },
+        {
+            "input": ['cable', 'a', 'b'],
+            "answer": True
+        },
+        {
+            "input": ['copyrightable', 'o', 'p'],
+            "answer": True
+        },
+        {
+            "input": ['copyrightable', 'o', 'a'],
+            "answer": False
+        },
+        {
+            "input": ['copyrightable', 'o', 'o'],
+            "answer": False
+        },
+    ]
+}
